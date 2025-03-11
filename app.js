066284253e6ec6f62b46db2640abd38eba234590
Deepak Gangwar <deepakgangwar321@gmail.com>
2025-03-11 16:26:15 +0530

    Initial commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { StreamClient } from "@stream-io/node-sdk";
 // dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors({
     origin:"*",
     methods:["GET,POST"],
